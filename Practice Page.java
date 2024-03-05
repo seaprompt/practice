c248b7f8e910931d7b4c70cf8cc2e867aa224797
seaprompt <joe.r.devault@gmail.com>
2024-03-04 22:46:28 -0500

    feature one update 1

--- Practice Page.java
+++ Practice Page.java
@@ -3,5 +3,19 @@ System.out.println("line 2 ");
 //test git
 //update page practice
 
+void test() {
+    System.out.println("test");
+}
 
+
+class Planet {
+    void revolve() {
+        System.out.println("Revolve");
+    }
+}
+
+Planet earh = new planet();
+
+
+Planet venus = new planet();
 
